62234d335ce0b0b851655ad0ec43c8e1f1779b64
dnieln7 <dnieln7@gmail.com>
2022-10-20 14:55:43 -0500

    Tutorial -> Authenticating users -> Coding the repositories

--- app/src/main/java/com/rookmotion/rooktraining/data/repository/UserRepository.kt
+++ app/src/main/java/com/rookmotion/rooktraining/data/repository/UserRepository.kt
@@ -3,6 +3,7 @@ package com.rookmotion.rooktraining.data.repository
 import com.rookmotion.app.sdk.RM
 import com.rookmotion.app.sdk.persistence.entities.user.RMUser
 import com.rookmotion.kotlin.sdk.utils.isValid
+import timber.log.Timber
 
 class UserRepository(private val rm: RM) {
 
@@ -12,6 +13,16 @@ class UserRepository(private val rm: RM) {
                 onSuccess(rmUser)
             } else {
                 onError(rmResponse.message)
+            }
+        }
+    }
+
+    fun syncIndexes() {
+        rm.syncUserIndexes { rmResponse, _ ->
+            if (rmResponse.isSuccess) {
+                Timber.i("Success syncing indexes")
+            } else {
+                Timber.e("Default indexes will be used, error: ${rmResponse.message}")
             }
         }
     }
