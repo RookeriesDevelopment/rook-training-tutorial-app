0723ce5a9e708b8d1c6760692f4341f6496eb42e
dnieln7 <dnieln7@gmail.com>
2022-10-21 12:14:20 -0500

    Tutorial -> Authenticating users -> Helpers

--- app/src/main/java/com/rookmotion/rooktraining/utils/RMExtensions.kt
+++ app/src/main/java/com/rookmotion/rooktraining/utils/RMExtensions.kt
@@ -1,7 +1,17 @@
 package com.rookmotion.rooktraining.utils
 
 import android.app.Activity
+import android.content.Context
+import android.widget.Toast
 import com.rookmotion.rooktraining.RookTrainingApplication
 import com.rookmotion.rooktraining.rm.RMServiceLocator
 
 val Activity.rmLocator: RMServiceLocator get() = (application as RookTrainingApplication).rmServiceLocator
+
+fun Context.toastShort(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.toastLong(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+}
